f2e02c20a680dc38bf5fb003604001893b06d6b6
vladimir360 <vladimir360@mail.ru>
2024-04-08 20:51:02 +0300

    2 task

---
+++ 2/index.js
@@ -0,0 +1,10 @@
+// Задание 1
+let temperatureCelsius = 25;  // Температура в градусах Цельсия
+let temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;  // Перевод в градусы Фаренгейта
+console.log("Температура в Цельсиях:", temperatureCelsius);
+console.log("Температура в Фаренгейтах:", temperatureFahrenheit);
+
+// Задание 2
+let name = "Ваше_имя";
+let admin = name;  // Присвоение значения переменной name переменной admin
+console.log(admin);
